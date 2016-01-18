f5e658a60b6d8286261cbea22f6c205671b5e0b0
molebot <molebot@qq.com>
2016-01-18 17:48:42 +0800

    bug fix

--- py/ctpEngine.py
+++ py/ctpEngine.py
@@ -159,6 +159,8 @@ class SymbolOrdersManager:
             if _data['UpdateTime'][:4] == '14:5':
                 self.me.dictProduct[self.productid][self.symbol] = _data['Volume']
                 self.me.set_instrument()
+                if _data['Volume']<max(self.me.dictProduct[self.productid].values()):
+                    return
             if self.me.socket:
                 if (self.symbol,self.exchange) not in self.me.subInstrument:
                     self.__hold = 0
@@ -478,6 +480,10 @@ class MainEngine:
                 for _inst,_prod in _all:
                     for _instr,_v in self.dictProduct[_prod].items():
                         _exchangeid = self.dictInstrument.get(_instr,{}).get("ExchangeID",'')
+                        event = Event(type_=EVENT_LOG)
+                        log = u'订阅[%s]以分析主力合约持仓'%_instr
+                        event.dict_['log'] = log
+                        self.ee.put(event)
                         self.subscribe(_instr,_exchangeid)
                 self.master = {}
 
